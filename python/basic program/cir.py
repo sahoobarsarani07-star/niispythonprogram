cf5b566f378c07293522e1e2e0bd645844c39a3b
Barsa <sahoobarsarani07@gmail.com>
2026-02-22 13:19:19 +0545

    basic program

---
+++ python/basic program/cir.py
@@ -0,0 +1,12 @@
+#write a python program to calculate the area of circle .
+print("enter radius")
+r=float(input())
+area=3.14*r*r 
+print("area=",area)
+
+
+#write a python program to calculate circumstances of a circle.
+radius=float(input("Enter radius of the circle:"))
+circumstance=2*math.pi*radius
+print("circumstance of the circle is:",circumstance)
+
---
+++ python/basic program/rec.py
@@ -0,0 +1,15 @@
+#write a program to calculate the area of a rectangle.
+print("enter rectangle length")
+L=int(input())
+print("enter rectangle breadth")
+B=int(input())
+ar=L*B
+print("area=",ar)
+
+#write a python program to calculate the perimeter of a rectangle.
+print("enter length")
+L=int(input())
+print("enter breath")
+B=int(input())
+pr=2*(L+b)
+
