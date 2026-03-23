6f9468e2c3ea283ae4738658514a0c11faa5cbbf
Barsa <sahoobarsarani07@gmail.com>
2026-03-23 20:45:49 +0530

    constructer

---
+++ python/constructer/c1.py
@@ -0,0 +1,18 @@
+class Rectangle:
+    def __init__(self):
+        self.length = 5
+        self.breadth = 7
+
+    def show(self):
+        print("rectangle length =", self.length)
+        print("rectangle breadth =", self.breadth)
+
+    def area(self):
+        print("area of rectangle =", self.length * self.breadth)
+
+    def perimeter(self):
+        return 2 * (self.length + self.breadth)
+r = Rectangle()
+r.show()
+r.area()
+print("perimeter of rectangle =", r.perimeter())
